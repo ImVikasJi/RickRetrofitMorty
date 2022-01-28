cf3390d62489810594d48568743b49c892462c83
ImVikasJi <vbhadoriya3@gmail.com>
2022-01-28 17:30:41 +0530

    Implemented Header to the UI using Epoxy Model, also learnt about groupBy{}

--- app/src/main/java/com/example/rickmorty/character/CharacterListActivity.kt
+++ app/src/main/java/com/example/rickmorty/character/CharacterListActivity.kt
@@ -7,13 +7,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.epoxy.EpoxyRecyclerView
 import com.example.rickmorty.R
 import com.example.rickmorty.databinding.ActivityCharacterListBinding
+import com.example.rickmorty.databinding.ActivityMainBinding
 import com.example.rickmorty.epoxy.CharacterListPagingEpoxyController
 import com.example.rickmorty.viewmodel.CharacterViewModel
 
 class CharacterListActivity : AppCompatActivity() {
 
-//    private var _binding: ActivityCharacterListBinding? = null
-//    private val binding get() = _binding!!
+    private var _binding: ActivityCharacterListBinding? = null
+    private val binding get() = _binding!!
 
     private val epoxyController = CharacterListPagingEpoxyController()
 
@@ -21,12 +22,18 @@ class CharacterListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_character_list)
+        _binding = ActivityCharacterListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         characterViewModel.characterPagedListLiveData.observe(this) { pageList ->
             epoxyController.submitList(pageList)
         }
             findViewById<EpoxyRecyclerView>(R.id.epoxyRecyclerView).setController(epoxyController)
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/rickmorty/epoxy/CharacterListPagingEpoxyController.kt
+++ app/src/main/java/com/example/rickmorty/epoxy/CharacterListPagingEpoxyController.kt
@@ -1,12 +1,13 @@
 package com.example.rickmorty.epoxy
 
-import com.airbnb.epoxy.EpoxyController
 import com.airbnb.epoxy.EpoxyModel
 import com.airbnb.epoxy.paging.PagedListEpoxyController
 import com.example.rickmorty.R
 import com.example.rickmorty.databinding.ModelCharacterListItemBinding
+import com.example.rickmorty.databinding.ModelCharacterListTitleBinding
 import com.example.rickmorty.model.GetCharacterByIdResponse
 import com.squareup.picasso.Picasso
+import java.util.*
 
 class CharacterListPagingEpoxyController : PagedListEpoxyController<GetCharacterByIdResponse>() {
     override fun buildItemModel(
@@ -16,10 +17,39 @@ class CharacterListPagingEpoxyController : PagedListEpoxyController<GetCharacter
         return CharacterGridItemEpoxyModel(item!!.image, item.name).id(item.id)
     }
 
+    override fun addModels(models: List<EpoxyModel<*>>) {
+        if (models.isEmpty()) {
+            LoadingEpoxyModel().id("loading").addTo(this)
+            return
+        }
+        // This Header will show 5 main family members of Rick and morty
+        CharacterGridTitleEpoxyModel("Main Family")
+            .id("main_family_header")
+            .addTo(this)
+
+        // sublist will take index from 0 to n-1
+        super.addModels(models.subList(0, 5))
+
+        // This sublist will take members from index 5 to size-1 ,
+        // as a List of CharacterGridTitleEpoxyModel
+        // groupBy will return a list of each model with the value we have given as a map
+        (models.subList(5, models.size) as List<CharacterGridItemEpoxyModel>).groupBy {
+            it.name[0].toString().uppercase(Locale.US)
+        }.forEach { mapEntry ->
+            val character = mapEntry.key.uppercase(Locale.US)
+            CharacterGridTitleEpoxyModel(character)
+                .id(character)
+                .addTo(this)
+
+            super.addModels(mapEntry.value)
+        }
+    }
+
+
     data class CharacterGridItemEpoxyModel(
         val imageUrl: String,
         val name: String
-    ): ViewBindingKotlinModel<ModelCharacterListItemBinding>(R.layout.model_character_list_item) {
+    ) : ViewBindingKotlinModel<ModelCharacterListItemBinding>(R.layout.model_character_list_item) {
 
         override fun ModelCharacterListItemBinding.bind() {
             Picasso.get().load(imageUrl).into(characterImageView)
@@ -27,4 +57,17 @@ class CharacterListPagingEpoxyController : PagedListEpoxyController<GetCharacter
         }
     }
 
+    data class CharacterGridTitleEpoxyModel(
+        val title: String
+    ) : ViewBindingKotlinModel<ModelCharacterListTitleBinding>(R.layout.model_character_list_title) {
+        override fun ModelCharacterListTitleBinding.bind() {
+            tvHeaderTextView.text = title
+        }
+
+        // This will set the length of the header to the span count return by the func()
+        override fun getSpanSize(totalSpanCount: Int, position: Int, itemCount: Int): Int {
+            return totalSpanCount
+        }
+
+    }
 }
